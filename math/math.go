1939c057543ea51c9cb23710a0719d032661d02b
Roy Andrew <hello@royandrew.dev>
2025-04-26 18:03:35 +0100

    Add documentation

--- math/math.go
+++ math/math.go
@@ -1,5 +1,11 @@
 package goch10mod
 
+// This package provide functions to perform mathematical operations
+
+// Add function adds two integers together and returns the result.
+// See [Addition]
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(v1 int, v2 int) int {
 	return v1 + v2
 }
